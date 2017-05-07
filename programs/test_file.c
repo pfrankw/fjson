516500be1f7026c4a088522597fe2ee114f6e8bf
Francesco Pompò <pfrankw@users.noreply.github.com>
2017-05-07 22:45:30 +0200

    Modified style

--- programs/test_file.c
+++ programs/test_file.c
@@ -5,72 +5,72 @@
 
 #include <fjson/fjson.h>
 
-void print_el( fjson_element_t *el )
+void print_el(fjson_element_t *el)
 {
-    if( !el )
+    if (!el)
         return;
 
-    switch( el->type ){
+    switch (el->type) {
 
-        case FJSON_TYPE_OBJECT:{
+    case FJSON_TYPE_OBJECT: {
 
-            fjson_pair_t *cur_pair = el->pairs;
+        fjson_pair_t *cur_pair = el->pairs;
 
-            printf("{ ");
+        printf("{ ");
 
-            while( cur_pair ){
-                printf("\"%s\": ", cur_pair->key->str );
-                print_el( cur_pair->value );
-                cur_pair = cur_pair->next;
-                if( cur_pair )
-                    printf(", ");
-            }
-
-            printf(" }");
-            break;
+        while (cur_pair) {
+            printf("\"%s\": ", cur_pair->key->str);
+            print_el(cur_pair->value);
+            cur_pair = cur_pair->next;
+            if (cur_pair)
+                printf(", ");
         }
 
-        case FJSON_TYPE_ARRAY:{
+        printf(" }");
+        break;
+    }
 
-            fjson_array_t *cur = el->array;
+    case FJSON_TYPE_ARRAY: {
 
-            printf("[ ");
+        fjson_array_t *cur = el->array;
 
-            while( cur ){
-                print_el( cur->el );
-                cur = cur->next;
-                if( cur )
-                    printf(", ");
-            }
-            printf(" ]");
-            break;
+        printf("[ ");
+
+        while (cur) {
+            print_el(cur->el);
+            cur = cur->next;
+            if (cur)
+                printf(", ");
         }
+        printf(" ]");
+        break;
+    }
 
-        case FJSON_TYPE_STRING:
-            printf("\"%s\"", el->str);
-            break;
+    case FJSON_TYPE_STRING:
+        printf("\"%s\"", el->str);
+        break;
 
-        case FJSON_TYPE_NULL:
-            printf("null");
-            break;
+    case FJSON_TYPE_NULL:
+        printf("null");
+        break;
 
-        case FJSON_TYPE_BOOLEAN:
-            if(el->bool_val)
-                printf("true");
-            else
-                printf("false");
-            break;
+    case FJSON_TYPE_BOOLEAN:
+        if (el->bool_val)
+            printf("true");
+        else
+            printf("false");
+        break;
 
-        case FJSON_TYPE_NUMBER:
-            printf("%lld", el->num);
-            break;
+    case FJSON_TYPE_NUMBER:
+        printf("%lld", el->num);
+        break;
 
-        default:
-            break;
+    default:
+        break;
     }
 }
 
-int main( int argc, char **argv )
+int main(int argc, char **argv)
 {
 
     int r = -1;
@@ -79,29 +79,29 @@ int main( int argc, char **argv )
     char *json_buf = 0;
     size_t json_len = 0;
 
-    fp = fopen( argv[1], "rb" );
-    if( !fp )
+    fp = fopen(argv[1], "rb");
+    if (!fp)
         goto cleanup;
 
-    fseek( fp, 0, SEEK_END );
-    json_len = ftell( fp );
-    rewind( fp );
+    fseek(fp, 0, SEEK_END);
+    json_len = ftell(fp);
+    rewind(fp);
 
-    json_buf = calloc( 1, json_len );
+    json_buf = calloc(1, json_len);
 
-    if( fread( json_buf, 1, json_len, fp ) != json_len )
+    if (fread(json_buf, 1, json_len, fp) != json_len)
         goto cleanup;
 
     if (fjson_putbuf(fjson, json_buf, json_len) != 1)
         goto cleanup;
 
-    print_el( fjson->el );
+    print_el(fjson->el);
 
 
     r = 0;
 cleanup:
-    free( json_buf );
-    if( fp ) fclose( fp );
-    fjson_free( fjson, 1 );
+    free(json_buf);
+    if (fp) fclose(fp);
+    fjson_free(fjson, 1);
     return r;
 }
